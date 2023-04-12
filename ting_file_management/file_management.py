e3e3ed252eddb5dc6b4d990d23650510dd161382
LuccaRendall <luccarendall2@gmail.com>
2023-04-12 06:08:56 -0300

    Feat: Método txt.importer - error 2/3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if os.path.splitext(path_file)[1] == ".txt":
+        try:
+            with open(path_file, 'r') as file:
+                file_data = file.read()
+                return file_data.split('')
+        except FileNotFoundError:
+            return f"Arquivo {path_file} não encontrado", file
+    else:
+        sys.stderr.write('Formato inválido')
