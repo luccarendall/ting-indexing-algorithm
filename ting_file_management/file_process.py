95f85e9ec73a7db36a86eda4d81544f3c81fee0c
LuccaRendall <luccarendall2@gmail.com>
2023-04-12 06:17:02 -0300

    Feat: Método process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for position in range(len(instance)):
+        if path_file in instance.search(position).values():
+            return None
+
+    file_data = txt_importer(path_file)
+    lines = len(file_data)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": lines,
+        "linhas_do_arquivo": file_data
+    }
+
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
+    # https://www.geeksforgeeks.org/how-to-print-to-stderr-and-stdout-in-python/
 
 
 def remove(instance):
